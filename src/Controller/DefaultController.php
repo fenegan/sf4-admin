3fdd9f177b3e7b84577ff6dd68a9d00302891a3c
Killian Vermersch <killian@idequanet.com>
2017-12-19 12:20:59 +0100

    Added Happy Message Service

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,9 +4,21 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Service\MessageGenerator;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/happy")
+     */
+    public function happy(MessageGenerator $messageGenerator)
+    {
+        $message = $messageGenerator->getHappyMessage();
+
+        return new Response(
+            '<html><body>'.$message.'</body></html>'
+        );
+    }
     /**
      * @Route("/lucky/number")
      */
